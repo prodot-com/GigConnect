215b587be2f1da8bcf1424dbe2b367f375bfcac6
Probal Ghosh <probalghoshofficial@gmail.com>
2025-08-10 18:25:03 +0530

    Integrated tailwindCss

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h4>Zaalima Internship</h4>
+      <h4 className='text-indigo-700 flex 
+      h-screen justify-center items-center
+      font-bold text-4xl'>Zaalima Internship</h4>
     </div>
   )
 }
