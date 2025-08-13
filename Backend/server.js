21f0886ba8633cf8ab86584e68cc88f71523ae01
Probal Ghosh <probalghoshofficial@gmail.com>
2025-08-13 18:26:34 +0530

    Started express app

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,14 @@
 import connect_db from "./src/db/Index.js";
+import express from 'express'
+import { app } from "./App.js";
 
 connect_db()
+.then(()=>{
+    app.listen(process.env.PORT || 9000, ()=>{
+        console.log(`App listening at port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log('Connection failed', err)
+})
+
