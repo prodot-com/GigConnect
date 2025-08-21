406b0956f590d86276d637c73d57971e50be3743
Probal Ghosh <probalghoshofficial@gmail.com>
2025-08-22 00:04:50 +0530

    checking render backend deployment

--- Backend/App.js
+++ Backend/App.js
@@ -10,6 +10,9 @@ const app = express();
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send("Socket.io backend running")
+})
 
 app.use('/api/auth', authRoutes);
 app.use('/api/gigs', gigRoutes);
